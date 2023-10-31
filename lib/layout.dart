d7aa3da06a3633764eb1b2db98d66257d2e52de3
Jorge A. Mogotocoro F <jorgemogotocoro05@outlook.es>
2023-10-31 13:30:39 -0500

    feat: The LayoutFormat and LayoutPixelFormat classes are exposed

--- lib/layout.dart
+++ lib/layout.dart
@@ -11,3 +11,5 @@ export 'src/formats/boostrap_format.dart';
 export 'src/formats/physical_pixel/landscape_physical_pixel_layout_format.dart';
 export 'src/formats/physical_pixel/portrait_physical_pixel_layout_format.dart';
 export 'src/adaptive_builder.dart';
+export 'src/format.dart';
+export 'src/pixel.dart';
